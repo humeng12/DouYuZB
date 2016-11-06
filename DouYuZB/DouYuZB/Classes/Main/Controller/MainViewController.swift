733ae69954138416133f8a82fb54219f1a309a00
humeng12 <936608021@qq.com>
2016-11-06 18:24:22 +0800

    文件分类

---
+++ DouYuZB/DouYuZB/Classes/Main/Controller/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by 胡猛 on 16/11/6.
+//  Copyright © 2016年 HuMeng. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVc("Home");
+        addChildVc("Live");
+        addChildVc("Follow");
+        addChildVc("My");
+    }
+    
+    
+    private func addChildVc(storyName:String){
+    
+        
+        let childVc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!;
+        
+        addChildViewController(childVc);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
